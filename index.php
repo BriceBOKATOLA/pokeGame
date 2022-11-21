028d35f94d0eaa73f0d7f47128c75bb9dcaa5279
BriceBOKATOLA <91880016+BriceBOKATOLA@users.noreply.github.com>
2022-11-21 14:15:45 +0100

    Récupération des éléments dans l'api

--- index.php
+++ index.php
@@ -1,41 +1,49 @@
 <!-- test  -->
 <?php
-$pokemon = '150';
+// $pokemon = '150';
 
-$api = curl_init("https://pokeapi.co/api/v2/pokemon/$pokemon");
-curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
-$reponse = curl_exec($api);
-curl_close($api);
-$json = json_decode($reponse);
+// $api = curl_init("https://pokeapi.co/api/v2/pokemon/$pokemon");
+// curl_setopt($api, CURLOPT_RETURNTRANSFER, true);
+// $reponse = curl_exec($api);
+// curl_close($api);
+// $json = json_decode($reponse);
 
-echo'<h1>Pokemon</h1>';
+// echo'<h1>Pokemon</h1>';
 
-foreach($json->abilities as $k => $v){
-    echo $v->ability->name.'<br>';
-}
-echo '<h2>image</h2>';
-// dans les deux faces 
+// foreach($json->abilities as $k => $v){
+//     echo $v->ability->name.'<br>';
+// }
+// echo '<h2>image</h2>';
+// // dans les deux faces 
 
-// devant 
-echo'<img src="'.$json->sprites->back_default.'"width="200">';
-// derriere 
-echo'<img src="'.$json->sprites->front_default.'"width="200">';
+// // devant 
+// echo'<img src="'.$json->sprites->back_default.'"width="200">';
+// // derriere 
+// echo'<img src="'.$json->sprites->front_default.'"width="200">';
+// // <!-- recuperé tout le tableau  -->
+// $base = "https://pokeapi.co/api/v2/pokemon/";
+// $id = 1;
+// $data = file_get_contents($base,$id.'/');
+// $pokemon = json_decode($data);
+// var_dump($pokemon);
+
+// Recuperation des elements de l'api 
 // <!-- recuperé tout le tableau  -->
 $base = "https://pokeapi.co/api/v2/pokemon/";
-$id = 1;
-$data = file_get_contents($base,$id.'/');
-$pokemon = json_decode($data);
+for ($id = 1; $id <= 10 ; $id++) { 
+    $data = file_get_contents($base.$id.'/');
+    $pokemon = json_decode($data);
+    echo $pokemon->name."<br>";
+// recuperation de l'image
+    echo'<img src="'.$pokemon->sprites->front_default.'"width="200">';
+}
+
 var_dump($pokemon);
 
 ?>
 <!-- fin du test  -->
 
 
-
-<?php 
-require_once'./function.php';
-
-?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
