4820fabc2d7a3a1b088cbcd81e81685ea4d8b911
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-11-21 13:13:03 +0100

    class pokemon

--- classes/Pokemon.php
+++ classes/Pokemon.php
@@ -21,4 +21,92 @@ class Pokemon {
      * @var int
      */
     protected $health;
+
+    /**
+     * Constructeur de la classe Pokemon
+     * @param string $type
+     * @param string $name
+     * @param integer $health
+     */
+    public function __construct(string $type, string $name, int $health)
+    {
+        $this->type = $type;
+        $this->name = $name;
+        $this->health = $health;
+    }
+
+
+    // --- METHODS ---
+
+
+    // GETTER
+
+    /**
+     * Récupère le type du pokemon
+     *
+     * @return string
+     */
+    protected function getType():string
+    {
+        return $this->type;
+    }
+
+    /**
+     * Récupère le nom du pokemon
+     *
+     * @return string
+     */
+    protected function getName():string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Récupère la vie du pokemon
+     *
+     * @return int
+     */
+    protected function getHealth():int
+    {
+        return $this->health;
+    }
+
+
+    // SETTER
+
+    /**
+     * Renseigne le type du pokémon
+     *
+     * @param string $type
+     * @return self
+     */
+    public function setType(string $type):self
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * Renseigne le nom du pokémon
+     *
+     * @param string $name
+     * @return self
+     */
+    public function setName(string $name):self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * Renseigne la vie du pokémon
+     *
+     * @param int $health
+     * @return self
+     */
+    public function setHealth(string $health):self
+    {
+        $this->health = $health;
+        return $this;
+    }
 }
