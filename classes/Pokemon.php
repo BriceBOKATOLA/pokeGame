e6feac70a683f4300401b65777d85fef2c56b632
AlexandreBIEN <91879953+AlexandreBIEN@users.noreply.github.com>
2022-11-21 12:36:11 +0100

    début class + diagramme UML

---
+++ classes/Pokemon.php
@@ -0,0 +1,24 @@
+<?php
+
+class Pokemon {
+
+    // --- ATTRIBUTS --- 
+
+    /**
+     * Type du pokémon
+     * @var string
+     */
+    protected $type;
+
+    /**
+     * Nom du pokémon
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * Vie du pokémon
+     * @var int
+     */
+    protected $health;
+}
